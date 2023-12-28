7cc56570dd26d2efc5f923bfd4c7ffc32a51e443
Daniel du Preez <dandupreez@legiondev.co.za>
2023-12-28 15:17:56 +0200

    Add description on the new columns

--- main.py
+++ main.py
@@ -13,3 +13,8 @@ with col2:
     Write something about yourself
     '''
     st.info(content)
+
+content2 = '''
+Below you can find some of the apps I have built in Python. Feel free to contact me!
+'''
+st.write(content2)
